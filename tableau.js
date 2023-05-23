53d8b13ba3e0f36066e1036df3d0694577eba34e
maelb <mael.besrest@edu.ece.fr>
2023-05-23 13:04:20 +0200

    dsjkhfz

--- tableau.js
+++
@@ -1,6 +0,0 @@
-function supprimerLigne(button) {
-            if (confirm("Êtes-vous sûr de vouloir supprimer cette ligne ?")) {
-                var row = button.parentNode.parentNode;
-                row.parentNode.removeChild(row);
-            }
-        }
