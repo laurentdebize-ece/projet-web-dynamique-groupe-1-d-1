124be9d7dfb0d343330c3da62b089ee61a6178f0
EvaAfonso <eva.afonso@edu.ece.fr>
2023-05-23 21:41:43 +0200

    barre nav

--- PageAcquisEleveCompetence.php
+++ PageAcquisEleveCompetence.php
@@ -1,7 +1,19 @@
 <?php session_start() ?>
 <?php 
 
-    require ('./Global/Header.php');
+    require ('./Global/Header.php'); ?>
+
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="PageAcceuilProfesseur.php">Acceuil</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<?php
 
     $Classe = $_GET['classe'];
     $Promo = $_GET['promo'];
--- PageProf.php
+++ PageProf.php
@@ -2,6 +2,15 @@
 require('./Global/Header.php'); 
 
 ?>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="PageAcceuilProfesseur.php">Acceuil</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
 
 <?php
 
